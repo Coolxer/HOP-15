11c1c807d647a47781a44a6aa3837628abb523bf
Anwarus <Anwarus1289@gmail.com>
2017-12-28 18:46:49 +0100

    clear untracked files

--- Debug/input.cpp.d
+++
@@ -1,4 +0,0 @@
-C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\input.cpp.o: \
- C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\input.cpp \
- C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\input.h \
- C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\Config.h
--- Debug/sevSegms.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\sevSegms.cpp.o: \
- C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\sevSegms.cpp \
- C:\Users\admin\AppData\Local\Temp\VMBuilds\HOP-15\mega_atmega1280\Debug\sevSegms.h
