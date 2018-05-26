19bbee19f2f0a25c8fb0bd720e170df64fa3fb23
coolxer <lukasik4789@gmail.com>
2018-05-26 09:08:18 +0200

    fixed_repository

--- Release/sevSegms.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\lukas\AppData\Local\Temp\VMBuilds\hop-15\mega_atmega2560\Release\sevSegms.cpp.o: \
- C:\Users\lukas\AppData\Local\Temp\VMBuilds\hop-15\mega_atmega2560\Release\sevSegms.cpp \
- C:\Users\lukas\AppData\Local\Temp\VMBuilds\hop-15\mega_atmega2560\Release\sevSegms.h
